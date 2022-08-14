000b03fdbe5cb1ed5fb27022e31f86a8151f1c22
lulixue <jackreagan@163.com>
2022-08-14 20:42:23 +0800

    添加图片控件

--- app/src/main/java/com/liren/composetest/ImageTest.kt
+++ app/src/main/java/com/liren/composetest/ImageTest.kt
@@ -1,7 +1,10 @@
 package com.liren.composetest
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.view.*
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.TwoWayConverter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -16,12 +19,49 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.sqrt
+
+
+class OffsetValueAnimator(private val fromOffset: Offset, destX: Float, destY: Float, private val onOffsetChange: (Offset) -> Unit) {
+    private val deltaX = destX - fromOffset.x
+    private val deltaY = destY - fromOffset.y
+    private var animationDisposed = false
+    private val animator: ValueAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+        addUpdateListener {
+            val newOffset = Offset(fromOffset.x + deltaX * it.animatedFraction,
+                fromOffset.y + deltaY * it.animatedFraction)
+            println("from $fromOffset animate to $newOffset")
+            onOffsetChange(newOffset)
+            if (it.animatedFraction == 1f) {
+                animationDisposed = true
+            }
+        }
+        this.duration = 300L
+    }
+
+    fun isRunning() = animator.isRunning
+
+    fun stop() {
+        if (animator.isRunning) {
+            animator.pause()
+            animationDisposed = true
+        }
+    }
+
+    fun start() {
+        if (!animationDisposed) {
+            animator.start()
+        }
+    }
+}
 
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -31,12 +71,22 @@ fun ImageView() {
     var scaling by remember {
         mutableStateOf(false)
     }
+    var parentSize by remember {
+        mutableStateOf(Size.Zero)
+    }
     var imageSize by remember {
         mutableStateOf(Size.Zero)
     }
     var dragging by remember {
         mutableStateOf(false)
     }
+    var fromOffset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+    var valueAnimation by remember {
+        mutableStateOf<OffsetValueAnimator?>(null)
+    }
+
     val context = LocalContext.current
     var offset by remember { mutableStateOf(Offset.Zero) }
     val scaleListener by remember {
@@ -72,6 +122,10 @@ fun ImageView() {
     val scaleGestureDetector by remember {
         mutableStateOf(ScaleGestureDetector(context, scaleListener))
     }
+    fun imageShowWidth() = parentSize.width
+    fun imageShowHeight() = (parentSize.width / imageSize.width) * imageSize.height
+    fun getOffsetWidthLimit(percent: Float) = parentSize.width * scale * percent
+    fun getOffsetHeightLimit(percent: Float) = parentSize.height * scale * percent
 
     val dragListener by remember {
         mutableStateOf(object : View.OnTouchListener {
@@ -79,23 +133,58 @@ fun ImageView() {
             private var downY = 0f
             private var downOffset = Offset.Zero
             private val touchSlop get() = ViewConfiguration.getTouchSlop() * scale
+
+
+            private val criticalOffsetX get() = parentSize.width *  scale * 0.5f
+            private val criticalOffsetY get() = parentSize.height * (scale * 0.5f)
+            private val maxAnimationOffsetX get() = parentSize.width * (scale * 0.25f)
+            private val maxAnimationOffsetY get() = parentSize.height * (scale * 0.25f)
+            private val offsetXRange get() = -criticalOffsetX..criticalOffsetX
+            private val offsetYRange get() = -criticalOffsetY..criticalOffsetY
+
             @SuppressLint("ClickableViewAccessibility")
             override fun onTouch(v: View?, e: MotionEvent): Boolean {
                 when (e.actionMasked) {
                     MotionEvent.ACTION_DOWN -> {
+                        valueAnimation?.stop()
+                        valueAnimation = null
                         downX = e.x
                         downY = e.y
                         downOffset = offset
                     }
                     MotionEvent.ACTION_MOVE -> {
                         if (!scaling && e.pointerCount == 1) {
-
                             val new = Offset(x = e.x - downX, y = e.y - downY)
                             if (sqrt(new.x * new.x + new.y * new.y) >= touchSlop) {
                                 dragging = true
                             }
                             if (dragging) {
-                                offset = downOffset + new
+                                val newOffset = downOffset + new
+                                var newOffsetX = newOffset.x
+                                var newOffsetY = newOffset.y
+                                var overflow = false
+
+                                if (newOffset.x !in offsetXRange) {
+                                    val overflowLT = newOffset.x > criticalOffsetX
+                                    overflow = true
+                                    newOffsetX = if (overflowLT) min(maxAnimationOffsetX, newOffset.x) else
+                                                    max(-maxAnimationOffsetX, newOffset.x)
+                                }
+                                if (newOffset.y !in offsetYRange) {
+                                    overflow = true
+                                    newOffsetY = if (newOffset.y > criticalOffsetY) min(maxAnimationOffsetY, newOffset.y)
+                                                else max(-maxAnimationOffsetY, newOffset.y)
+
+                                }
+
+                                offset = Offset(newOffsetX, newOffsetY)
+                                if (overflow) {
+                                    val destOffsetX = max(min(newOffsetX, criticalOffsetX), -criticalOffsetX)
+                                    val destOffsetY = max(min(newOffsetY, criticalOffsetY), -criticalOffsetY)
+                                    valueAnimation = OffsetValueAnimator(offset, destOffsetX, destOffsetY) {
+                                        offset = it
+                                    }
+                                }
                                 println("on drag $offset")
                             }
                         }
@@ -106,6 +195,11 @@ fun ImageView() {
                         downY = e.y
                         scaling = true
                     }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        valueAnimation?.also {
+                            it.start()
+                        }
+                    }
                 }
                 return true
             }
@@ -114,7 +208,14 @@ fun ImageView() {
     }
 
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .onGloballyPositioned {
+            if (parentSize.isEmpty()) {
+                parentSize = Size(it.size.width.toFloat(), it.size.height.toFloat())
+            }
+        },
+        contentAlignment = Alignment.Center) {
         Image(painter = painterResource(id = R.drawable.mengdian),
             contentDescription = null,
             modifier = Modifier
@@ -136,7 +237,8 @@ fun ImageView() {
                     scaleY = scale,
                     translationX = offset.x,
                     translationY = offset.y
-                ).background(Color.Blue)
+                )
+                .background(Color.Blue)
                 .onGloballyPositioned {
                     if (imageSize.isEmpty()) {
                         imageSize = Size(it.size.width.toFloat(), it.size.height.toFloat())
@@ -144,7 +246,7 @@ fun ImageView() {
                 }
                 .clipToBounds()
             ,
-
+            contentScale = ContentScale.FillWidth
         )
     }
 }
